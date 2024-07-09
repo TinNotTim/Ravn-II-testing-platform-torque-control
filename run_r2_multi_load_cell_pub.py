b4f2693ce8e901c84c605d3cc807266d784d538b
TinNotTim <tinchiang1997@gmail.com>
2024-07-08 17:17:19 -0700

    half way through improving the loadcell node

--- run_r2_multi_load_cell_pub.py
+++ run_r2_multi_load_cell_pub.py
@@ -12,25 +12,34 @@ lc_pub = rospy.Publisher('load_cells', sensor_msgs.msg.JointState, queue_size=1)
 ser = serial.Serial('/dev/ttyACM0', 115200)
 rospy.sleep(3)
 
+loadcell_num = 6
+
 while not rospy.is_shutdown():
-            
-    b = ser.readline()         # read a byte string
-    try:
-        string_n = b.decode()      # decode byte string into Unicode  
-        string = string_n.rstrip() # remove \n and \r
-    except:
-        continue
     
-    try:
-        [reading_1, reading_2, reading_3, reading_4, reading_5, reading_6] = string.split('_')
-    except:
-        continue
+    if ser.in_waiting >= loadcell_num * size(int32):
+        byte_arry = ser.read(loadcell_num * size(int32))         # read a byte array of 6 int32
+
+        # Decode the byte array into int32
+        for i in range(loadcell_num):
+            
 
 
-    new_line = np.array([float(reading_1), float(reading_2), float(reading_3), float(reading_4), float(reading_5), float(reading_6)])
-    msg = sensor_msgs.msg.JointState()
-    msg.header.stamp = rospy.Time.now()
-    msg.position[:] = new_line.flat
-    lc_pub.publish(msg)
+        try:
+            string_n = b.decode()      # decode byte string into Unicode  
+            string = string_n.rstrip() # remove \n and \r
+        except:
+            continue
+        
+        try:
+            [reading_1, reading_2, reading_3, reading_4, reading_5, reading_6] = string.split('_')
+        except:
+            continue
+
+
+        # new_line = np.array([float(reading_1), float(reading_2), float(reading_3), float(reading_4), float(reading_5), float(reading_6)])
+        msg = sensor_msgs.msg.JointState()
+        msg.header.stamp = rospy.Time.now()
+        msg.position[:] = new_line.flat
+        lc_pub.publish(msg)
 
 
